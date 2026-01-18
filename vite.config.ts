40b265c713b8d8cd19a5326137d46ae0ead62505
coderafeng <coderafeng@qq.com>
2026-01-18 19:28:53 +0800

    feat:pnpm run build:skip-type-check

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
     plugins: [vue()],
-    base: 'web-demo-case-platform',
+    base: '/web-demo-case-platform/',
     resolve: {
         alias: {
             '@': resolve(__dirname, 'src')
